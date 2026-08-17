0fd4afa3c2c6c4cb44acaf77dee4bb61b60ba3f4
Water Chika <water_chika@outlook.com>
2026-08-17 14:49:03 +0800

    recreate surface if size changed

--- window.cpp
+++ window.cpp
@@ -194,16 +194,91 @@ private:
 };
 
 template<typename T>
+class register_size_change_callback : public T {
+public:
+    using parent = T;
+    using this_type = register_size_change_callback<T>;
+    register_size_change_callback(const configure auto& conf) : parent{conf} {
+        parent::set_size_changed_callback(size_changed_callback, this);
+    }
+    static void size_changed_callback(int width, int height, void* data) {
+        std::cout << "size changed" << std::endl;
+        auto t = reinterpret_cast<this_type*>(data);
+        t->size_changed(width, height);
+    }
+    void size_changed(int width, int height) {
+        parent::recreate_surface();
+    }
+};
+
+template<typename T>
+class add_recreate_surface_for : public T {
+public:
+    using parent = T;
+    add_recreate_surface_for(const configure auto& conf) : parent{conf} {
+    }
+    void recreate_surface() {
+        parent::destroy();
+        parent::recreate_surface();
+        parent::create();
+    }
+};
+template<typename T>
+class add_dummy_recreate_surface : public T {
+public:
+    using parent = T;
+    add_dummy_recreate_surface(const configure auto& conf) : parent{conf} {
+    }
+    void recreate_surface() {
+    }
+};
+
+template<typename T>
 class set_bo_alloc_size : public T {
 public:
     using parent = T;
     set_bo_alloc_size(const configure auto& conf) : parent{conf}
     {}
     auto get_bo_alloc_size() {
-        auto fb_width = parent::get_fb_width();
-        auto fb_height = parent::get_fb_height();
-        return fb_width*fb_height*sizeof(uint32_t);
+        auto [width,height] = parent::get_surface_resolution();
+        return width*height*sizeof(uint32_t);
     }
+    auto get_dma_buf_size() {
+        return get_bo_alloc_size();
+    }
+};
+
+template<typename T>
+class attach_dma_buf_fd_to_surface : public T {
+public:
+    using parent = T;
+    attach_dma_buf_fd_to_surface(const configure auto& conf) : parent{conf}
+    {
+        create();
+    }
+    ~attach_dma_buf_fd_to_surface() {
+        destroy();
+    }
+
+    void create() {
+        auto [width,height] = parent::get_surface_resolution();
+        uint32_t dma_buf_fd = parent::get_dma_buf_fd();
+        auto dmabuf = parent::get_dmabuf();
+        buffer_params = zwp_linux_dmabuf_v1_create_params(dmabuf);
+        zwp_linux_buffer_params_v1_add(buffer_params, dma_buf_fd, 0, 0, width, 0, 0);
+        buffer = zwp_linux_buffer_params_v1_create_immed(buffer_params, width, height, DRM_FORMAT_XRGB8888, 0);
+        assert(buffer != nullptr);
+        auto surface = parent::get_wayland_surface();
+        wl_surface_attach(surface, buffer, 0, 0);
+        wl_surface_commit(surface);
+    }
+    void destroy() {
+        wl_buffer_destroy(buffer);
+        zwp_linux_buffer_params_v1_destroy(buffer_params);
+    }
+private:
+    zwp_linux_buffer_params_v1* buffer_params;
+    wl_buffer* buffer;
 };
 
 template<typename T>
@@ -211,68 +286,26 @@ class add_hip_draw : public T {
 public:
     using parent = T;
     add_hip_draw(const configure auto& conf) : parent{conf},
-        frame_ptr{},
         upload_ptr{}
     {
-        int fd = parent::get_drm_fd();
-        int ret = 0;
         auto fb_width = parent::get_fb_width();
         auto fb_height = parent::get_fb_height();
-        uint32_t width = fb_width, height = fb_height;
-        hipExternalMemory_t hip_memory{};
-        {
-            uint32_t dma_buf_fd{};
-            int ret = amdgpu_bo_export(parent::get_amdgpu_bo(), amdgpu_bo_handle_type_dma_buf_fd, &dma_buf_fd);
-            if (ret < 0) {
-                throw std::runtime_error{"amdgpu_bo_export failed"};
-            }
-            {
-                parent::add_dmabuf_fd(dma_buf_fd, 0, 0, width, 0, 0);
-                auto params = parent::get_dmabuf_params();
-                zwp_linux_buffer_params_v1_create(params, width, height, DRM_FORMAT_XRGB8888, 0);
-                auto display = parent::get_display();
-                wl_display_roundtrip(display);
-                auto buffer = parent::get_buffer();
-                while (buffer == nullptr) {
-                    std::cout << "buffer is null, retry" << std::endl;
-                    wl_display_roundtrip(display);
-                    buffer = parent::get_buffer();
-                }
-                assert(buffer != nullptr);
-                auto surface = parent::get_wayland_surface();
-                wl_surface_attach(surface, buffer, 0, 0);
-                wl_surface_commit(surface);
-                wl_display_roundtrip(display);
-            }
-            hipExternalMemoryHandleDesc desc = {};
-            desc.size = parent::get_bo_alloc_size();
-            desc.type = hipExternalMemoryHandleTypeOpaqueFd;
-            desc.handle.fd = dma_buf_fd;
-            ret = hipImportExternalMemory(&hip_memory, &desc);
-            if (ret != hipSuccess) {
-                throw std::runtime_error{std::format("hipImportExternalMemory failed: {}", ret)};
-            }
-        }
-        {
-            hipExternalMemoryBufferDesc desc = {};
-            desc.offset = 0;
-            desc.size = parent::get_bo_alloc_size();
-            desc.flags = 0;
-
-            ret = hipExternalMemoryGetMappedBuffer(reinterpret_cast<void**>(&frame_ptr), hip_memory, &desc);
-            if (ret != hipSuccess) {
-                throw std::runtime_error{std::format("hipExternalMemoryGetMappedBuffer failed: {}", ret)};
-            }
-        }
         hipHostMalloc(&upload_ptr, fb_width*fb_height*sizeof(uint32_t));
         assert(upload_ptr != nullptr);
     }
     void draw() {
         auto fb_width = parent::get_fb_width();
         auto fb_height = parent::get_fb_height();
+        auto [width,height] = parent::get_surface_resolution();
+        std::cout << std::format("resolution={}x{}", width, height) << std::endl;
+        auto hip_memory = parent::get_external_memory();
+        auto frame_ptr = reinterpret_cast<uint32_t*>(parent::get_external_memory_buffer());
+
         parent::get_rfb(std::span{reinterpret_cast<uint8_t*>(upload_ptr), fb_width*fb_height*sizeof(uint32_t)});
-        uint32_t width = fb_width, height = fb_height;
-        copy_frame<<<dim3(8,8,1), dim3(32,1,1),0>>>(frame_ptr, upload_ptr, width, height, (width+32*8-1)/(32*8), (height+1*8-1)/(1*8));
+        copy_frame<<<dim3(8,8,1), dim3(32,1,1),0>>>(
+                frame_ptr, width, height,
+                upload_ptr, fb_width, fb_height,
+                (width+32*8-1)/(32*8), (height+1*8-1)/(1*8));
         int ret = hipDeviceSynchronize();
         if (ret != hipSuccess) {
             throw std::runtime_error{std::format("hipDeviceSynchronize failed: {}", ret)};
@@ -286,19 +319,21 @@ public:
         wl_display_flush(display);
     }
     __global__
-    static void copy_frame(uint32_t* frame_ptr, uint32_t* src, uint32_t width, uint32_t height, uint32_t tile_width, uint32_t tile_height) {
+    static void copy_frame(
+            uint32_t* frame_ptr, uint32_t frame_width, uint32_t frame_height,
+            uint32_t* src, uint32_t width, uint32_t height,
+            uint32_t tile_width, uint32_t tile_height) {
         for (int y_ = 0; y_ < tile_height; y_++) {
             int y = y_ + (threadIdx.y+blockIdx.y*blockDim.y)*tile_height;
-            if (y >= height) break;
+            if (y >= frame_height) break;
             for (int x_ = 0; x_ < tile_width; x_++) {
                 int x = x_ + (threadIdx.x+blockIdx.x*blockDim.x)*tile_width;
-                if (x >= width) break;
-                frame_ptr[y*width+x] = src[y*width+x];
+                if (x >= frame_width) break;
+                frame_ptr[y*frame_width+x] = src[(y*height/frame_height)*width+x*width/frame_width];
             }
         }
     }
 private:
-    uint32_t* frame_ptr;
     uint32_t* upload_ptr;
 };
 
@@ -505,8 +540,19 @@ using draw_app =
     add_rfb_process_keysym<
     add_rfb_process_pointer<
     add_hip_draw<
+    register_size_change_callback<
+    add_recreate_surface_for<
+    attach_dma_buf_fd_to_surface<
+    add_recreate_surface_for<
+    hip_helper::add_external_memory_buffer<
+    add_recreate_surface_for<
+    hip_helper::add_external_memory<
+    add_recreate_surface_for<
+    drm_helper::add_dma_buf_fd_with_amdgpu_bo<
+    add_recreate_surface_for<
     drm_helper::add_amdgpu_bo<
     set_bo_alloc_size<
+    add_dummy_recreate_surface<
     drm_helper::add_amdgpu_device<
     drm_helper::add_drm_fd<
     add_rfb<
@@ -516,7 +562,7 @@ using draw_app =
     wayland_helper::add_wayland_surface<
     cpp_helper::add_logger<
     empty_class
-    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 ;
 
 using namespace std::literals;
